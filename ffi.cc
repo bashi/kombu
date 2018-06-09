a56927953cfb13956645258d7aa2811b8cf78f74
Kenichi Ishibashi <ishibashi.kenichi@gmail.com>
2018-06-09 11:01:22 +0900

    Implement woff2_to_ttf()

--- ffi.cc
+++ ffi.cc
@@ -1,5 +1,6 @@
 #include <emscripten.h>
 
+#include "woff2/decode.h"
 #include "woff2/encode.h"
 
 extern "C" {
@@ -10,11 +11,23 @@ size_t get_max_compressed_size(const uint8_t* data, size_t length) {
 }
 
 EMSCRIPTEN_KEEPALIVE
-int ttf_to_woff2(const uint8_t* data, size_t length, uint8_t* result, size_t result_length) {
+int32_t ttf_to_woff2(const uint8_t* data, size_t length, uint8_t* result, size_t result_length) {
   size_t out_length = result_length;
   if (!woff2::ConvertTTFToWOFF2(data, length, result, &out_length))
     return -1;
   return out_length;
+}
+
+EMSCRIPTEN_KEEPALIVE
+size_t get_uncompressed_size(const uint8_t* data, size_t length) {
+  return woff2::ComputeWOFF2FinalSize(data, length);
+}
+
+EMSCRIPTEN_KEEPALIVE
+int32_t woff2_to_ttf(uint8_t* result, size_t result_length, const uint8_t* data, size_t length) {
+  if (!woff2::ConvertWOFF2ToTTF(result, result_length, data, length))
+    return -1;
+  return result_length;
 }
 
 }
